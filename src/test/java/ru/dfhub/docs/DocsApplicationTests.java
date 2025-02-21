f37791e93cfdbd27e13bafdca2d94fb2310d2714
dertfin0 <dertfin@gmail.com>
2025-02-21 12:21:46 +0300

    Init

---
+++ src/test/java/ru/dfhub/docs/DocsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dfhub.docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
