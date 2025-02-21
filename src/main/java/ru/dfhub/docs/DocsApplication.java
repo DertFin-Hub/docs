fd656395b61d04af78f0fc73ec033aad126b03bb
dertfin0 <dertfin@gmail.com>
2025-02-21 12:31:49 +0300

    Enable lazy init

--- src/main/java/ru/dfhub/docs/DocsApplication.java
+++ src/main/java/ru/dfhub/docs/DocsApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DocsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DocsApplication.class, args);
+		SpringApplication app = new SpringApplication();
+		app.setLazyInitialization(true);
+		app.run(args);
 	}
-
 }
